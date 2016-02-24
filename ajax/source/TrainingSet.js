078bf3bc7bbfabd10d7afaf4ab89e8d7b76bbbc7
TodorBalabanov <todor.balabanov@gmail.com>
2016-02-24 14:09:20 +0200

    Working on constructor function.

--- ajax/source/TrainingSet.js
+++ ajax/source/TrainingSet.js
@@ -35,27 +35,39 @@
  *
  * @date 19 Dec 2010
  */
-function TrainingSet(set, rates, size) {
+function TrainingSet(rates, size, past, future) {
 
 	/**
 	 * Floating point factor to convert from floating point to integer.
 	 */
 	const FLOATING_POINT_FACTOR = 10000.0;
 
-	/**
-	 * Size of all parallel history arrays.
+	/*
+	 * It is not possible arrays size to be negative number.
 	 */
-	this.size = size;
-
-	/**
-	 * Array with rates.
-	 */
-	this.rates = rates;
+	if (size < 0) {
+		size = 0;
+	}
 
 	/**
 	 * Array with training examples.
 	 */
-	this.examples = new Array( size );
+	this.rates = new Array(size);
+
+	/**
+	 * Array with training examples.
+	 */
+	this.examples = new Array(size);
+
+	/**
+	 * Array with rates.
+	 */
+	for ( i = 0; i < size; i++) {
+		this.rates[i] = rates[size - i - 1];
+		this.examples[i] = new TrainingExample();
+	}
+
+	//TODO splitData(past, future);
 
 	/**
 	 * Split number digits in separate double values. Devide each double value by 10.
@@ -73,12 +85,11 @@ function TrainingSet(set, rates, size) {
 	 * @date 23 Dec 2010
 	 */
 	this.splitDigits = function(values, size, number) {
-		for (var i=size-1; i>=0; i--) {
+		for (var i = size - 1; i >= 0; i--) {
 			values[i] = ((number % 10) / 10.0);
 			number /= 10;
 		}
 	}
-
 	/**
 	 * Merge number digits in sigle number value. Multiply each double value by 10.
 	 *
@@ -101,67 +112,66 @@ function TrainingSet(set, rates, size) {
 		/*
 		 * Plus 0.5 is for rounding.
 		 */
-		for (var i=size-1; i>=0; i--) {
-			result += Math.floor((values[i]*10.0+0.5) * multiplier);
+		for (var i = size - 1; i >= 0; i--) {
+			result += Math.floor((values[i] * 10.0 + 0.5) * multiplier);
 			multiplier *= 10;
 		}
 
-		return( result );
+		return (result );
 	}
-
-//	/**
-//	 * Check for specific time record.
-//	 *
-//	 * @return True if time record is found, false otherwise.
-//	 *
-//	 * @author Daniel Chutrov
-//	 *
-//	 * @email d.chutrov@gmail.com
-//	 *
-//	 * @date 23 Dec 2010
-//	 */
-//	this.isTimeFound = function(time) {
-//		/*
-//		 * Left side is the begining. Right side is the end.
-//		 */
-//		var left = 0;
-//		var right = size-1;
-//		var middle;
-//
-//		/*
-//		 * Binary search.
-//		 */
-//		do {
-//			middle = (left + right) / 2;
-//			if (this.rates[middle].time == time) {
-//				return( true );
-//			} else if (left >= right) {
-//				return( false );
-//			} else if (time < this.rates[middle].time) {
-//				right = middle - 1;
-//			} else if (time > this.rates[middle].time) {
-//				left = middle + 1;
-//			}
-//		} while ( true );
-//
-//		return( false );
-//	}
-//
-//	/**
-//	 * Load splitted digits.
-//	 *
-//	 * @author Daniel Chutrov
-//	 *
-//	 * @email d.chutrov@gmail.com
-//	 *
-//	 * @date 20 Dec 2010
-//	 */
-//	this.splitData = function() {
-//		for (var i=0; i<size; i++) {
-//			splitDigits(examples[i].time, examples.NUMBER_OF_INPUT_SPLIT_DIGITS, rates[i].time);
-//
-//			//TODO Some mechanisum of changing predicted value should be implemented.
-//			splitDigits(examples[i].expected, examples.NUMBER_OF_OUTPUT_SPLIT_DIGITS, (FLOATING_POINT_FACTOR*(rates[i].open+rates[i].close)/2.0));
-//		}
-//	}
+	//	/**
+	//	 * Check for specific time record.
+	//	 *
+	//	 * @return True if time record is found, false otherwise.
+	//	 *
+	//	 * @author Daniel Chutrov
+	//	 *
+	//	 * @email d.chutrov@gmail.com
+	//	 *
+	//	 * @date 23 Dec 2010
+	//	 */
+	//	this.isTimeFound = function(time) {
+	//		/*
+	//		 * Left side is the begining. Right side is the end.
+	//		 */
+	//		var left = 0;
+	//		var right = size-1;
+	//		var middle;
+	//
+	//		/*
+	//		 * Binary search.
+	//		 */
+	//		do {
+	//			middle = (left + right) / 2;
+	//			if (this.rates[middle].time == time) {
+	//				return( true );
+	//			} else if (left >= right) {
+	//				return( false );
+	//			} else if (time < this.rates[middle].time) {
+	//				right = middle - 1;
+	//			} else if (time > this.rates[middle].time) {
+	//				left = middle + 1;
+	//			}
+	//		} while ( true );
+	//
+	//		return( false );
+	//	}
+	//
+	//	/**
+	//	 * Load splitted digits.
+	//	 *
+	//	 * @author Daniel Chutrov
+	//	 *
+	//	 * @email d.chutrov@gmail.com
+	//	 *
+	//	 * @date 20 Dec 2010
+	//	 */
+	//	this.splitData = function() {
+	//		for (var i=0; i<size; i++) {
+	//			splitDigits(examples[i].time, examples.NUMBER_OF_INPUT_SPLIT_DIGITS, rates[i].time);
+	//
+	//			//TODO Some mechanisum of changing predicted value should be implemented.
+	//			splitDigits(examples[i].expected, examples.NUMBER_OF_OUTPUT_SPLIT_DIGITS, (FLOATING_POINT_FACTOR*(rates[i].open+rates[i].close)/2.0));
+	//		}
+	//	}
 }
